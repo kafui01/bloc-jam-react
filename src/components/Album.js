51c4cd65686698e2e2cc2e38dda3b018604682bd
kafui <kafuistocks@gmail.com>
2018-10-03 16:41:33 -0400

    added formatTime to album

--- src/components/Album.js
+++ src/components/Album.js
@@ -82,6 +82,20 @@ handleTimeChange(e) {
  this.setState({ currentTime: newTime });
 }
 
+formatTime(seconds) {
+ if (isNaN(seconds)) { return "-:--"; }
+ const wholeSeconds = Math.floor(seconds);
+ const minutes = Math.floor(wholeSeconds / 60);
+ const remainingSeconds = wholeSeconds % 60;
+ let output = minutes + ':';
+ if (remainingSeconds < 10) {
+   output += '0';
+ }
+    output += remainingSeconds;
+    return output;
+}
+
+
   render() {
    return (
      <section className="album">
