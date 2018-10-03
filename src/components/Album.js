3e420cea69e5648a448bc18f9c50431a4d29f505
kafui <kafuistocks@gmail.com>
2018-10-03 08:09:04 -0400

    added playback functionalities

--- src/components/Album.js
+++ src/components/Album.js
@@ -10,8 +10,38 @@ class Album extends Component {
  });
 
  this.state = {
-   album: album
+   album: album,
+   currentSong: album.songs[0],
+   isPlaying: false
  };
+
+ this.audioElement = document.createElement('audio');
+ this.audioElement.src = album.songs[0].audioSrc;
+}
+
+play() {
+  this.audioElement.play();
+  this.setState({ isPlaying: true });
+}
+
+pause() {
+  this.audioElement.pause();
+  this.setState({ isPlaying: false });
+}
+
+setSong(song) {
+  this.audioElement.src = song.audioSrc;
+  this.setState({ currentSong: song });
+}
+
+handleSongClick(song) {
+  const isSameSong = this.state.currentSong === song;
+  if (this.state.isPlaying && isSameSong) {
+    this.pause();
+  } else {
+    if (!isSameSong) { this.setSong(song); }
+    this.play();
+  }
 }
 
  render() {
@@ -25,7 +55,9 @@ class Album extends Component {
          <div className="album-details">
             <h1 id="album-title">{this.state.album.title}</h1>
             <h2 className="artist">{this.state.album.artist}</h2>
-            <div id="release-info">{this.state.album.releaseInfo}   </div>
+            <div id="release-info">{this.state.album.releaseInfo}
+            
+            </div>
          </div>
        </section>
        <table id="song-list">
@@ -35,8 +67,12 @@ class Album extends Component {
            <col id="song-duration-column" />
          </colgroup>  
          <tbody>{this.state.album.songs.map((song, index) =>
-           <tr  key={index}>
-             <td>{index + 1}</td>
+           <tr  key={index} onClick={() => this.handleSongClick(song)} >
+             <td>
+               <span>{index + 1}</span>
+                <span className="ion-play"></span>
+                <span className="ion-pause"></span>
+             </td>
              <td>{song.title}</td>
              <td>{song.duration}</td>
            </tr>
